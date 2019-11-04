d78df13422f787b6520a306626109cc1b31b1981
web-dev <dias.07.04@gmail.com>
2019-11-04 12:41:39 +0600

    analytic center first commit

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+var myApp = angular.module('app', ['ui.bootstrap', 'ngSanitize', 'ui.select']);
+
+
+
+angular.module("app").controller("mainCtrl", function ($scope, $http, $timeout, $uibModal, $log) {
+
+
+
+
+});
+
+
+

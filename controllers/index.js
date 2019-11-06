d12f7c9b754bf8fd97223d52610ab5657d664abc
web-dev <dias.07.04@gmail.com>
2019-11-06 14:40:10 +0600

    analytic center first commit

--- controllers/index.js
+++ controllers/index.js
@@ -36,33 +36,45 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridGroupingConst
         enableFiltering: true,
         treeRowHeaderAlwaysVisible: false,
         columnDefs: [
-            { name: 'name', width: '*' },
-            { name: 'gender', grouping: { groupPriority: 1 }, sort: { priority: 1, direction: 'asc' }, width: '*', cellFilter: 'mapGender' },
-            { name: 'age', treeAggregationType: uiGridGroupingConstants.aggregation.MAX, width: '*' },
+            { name: 'groupName', displayName: 'Название групп', grouping: { groupPriority: 0 }, sort: { priority: 0, direction: 'asc' }, width: '*', cellFilter: 'mapGender' },
+            { name: 'subGroupName', displayName: 'Подгруппа', grouping: { groupPriority: 0 }, sort: { priority: 0, direction: 'desc' }, width: '*', cellTemplate: '<div><div ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" class="ui-grid-cell-contents" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>' },
+            { name: 'sliceNumber', displayName: 'Номер среза', width: '*' },
+            { name: 'period', displayName: 'Период', width: '*' },
+            { name: 'sliceRegion', displayName: 'Срез по региону', width: '*' },
+            { name: 'DB_number', displayName: '# в БД', width: '*' },
+            { name: 'formed', displayName: 'Сформирован', width: '*' }
+
+
+
+
+
+
+            /*{ name: 'age', treeAggregationType: uiGridGroupingConstants.aggregation.MAX, width: '*' },
             { name: 'company', width: '*' },
             { name: 'registered', width: '*', cellFilter: 'date', type: 'date' },
-            { name: 'state', grouping: { groupPriority: 0 }, sort: { priority: 0, direction: 'desc' }, width: '*', cellTemplate: '<div><div ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" class="ui-grid-cell-contents" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>' },
             { name: 'balance', width: '*', cellFilter: 'currency', treeAggregationType: uiGridGroupingConstants.aggregation.AVG, customTreeAggregationFinalizerFn: function( aggregation ) {
                     aggregation.rendered = aggregation.value;
-                } }
+                } }*/
         ],
         onRegisterApi: function( gridApi ) {
             $scope.gridApi = gridApi;
         }
     };
 
-    $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
+    // $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
+    $http.get('json/data.json')
         .then(function(response) {
             var data = response.data;
+            console.log(response.data);
 
-            for ( var i = 0; i < data.length; i++ ){
+            /*for ( var i = 0; i < data.length; i++ ){
                 var registeredDate = new Date( data[i].registered );
                 data[i].state = data[i].address.state;
                 data[i].gender = data[i].gender === 'male' ? 1: 2;
                 data[i].balance = Number( data[i].balance.slice(1).replace(/,/,'') );
                 data[i].registered = new Date( registeredDate.getFullYear(), registeredDate.getMonth(), 1 )
             }
-            delete data[2].age;
+            delete data[2].age;*/
             $scope.gridOptions.data = data;
         });
 

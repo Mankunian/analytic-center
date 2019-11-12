2c673e38aafefc24f714dd125f09a1526f3a37cd
web-dev <dias.07.04@gmail.com>
2019-11-12 11:20:27 +0600

    index page list box with checkbox tree-grid table

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,88 @@
-var app = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.grouping', 'angularjs-dropdown-multiselect', 'treeGrid']);
+var app = angular.module('app', ['ngTouch', 'treeGrid', "checklist-model"]);
 
-app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridGroupingConstants', function ($scope, $http, $interval, uiGridGroupingConstants) {
+app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
 
+    $scope.user = [];
+    $scope.checkAll = function (user) {
+        console.log(user);
+
+        $scope.group = user;
+
+
+        console.log(dateFromString);
+        console.log(dateToString);
+        console.log($scope.statsrez);
+        console.log($scope.group);
+
+        var dataObj = {
+            "startDate": dateFromString,
+            "endDate": dateToString,
+            "maxRecNum": $scope.statsrez,
+            // "region": 19,
+            "groups": $scope.group
+        };
+
+        console.log(dataObj);
+
+        $http({
+            method: 'POST',
+            url: 'http://18.140.232.52:8081/api/v1/slices',
+            data: dataObj
+        }).then(function (value) {
+            console.log(value)
+        }, function (reason) {
+            console.log(reason)
+        })
+
+    };
+
+
+//Получить № статсреза
+    $scope.getStatSrez = function () {
+        $http({
+            method: 'GET',
+            url: 'http://18.140.232.52:8081/api/v1/slices/max'
+        }).then(function (value) {
+            $scope.statsrez = value.data.value;
+            console.log($scope.statsrez);
+        })
+    };
+
+    $scope.getStatSrez();
+    //Получить № статсреза
+
+
+    //Дата начала отчета по умолчанию 1 января 2019
     var fromTimestamp = 1546322400;
     $scope.dateFrom = new Date(fromTimestamp * 1000);
-    console.log($scope.dateFrom);
+    console.log($scope.dateFrom.getTime() / 1000);
 
     $scope.dateTo = new Date();
 
+
+    console.log($scope.dateFrom);
+    console.log($scope.dateTo);
+
+
+    var dd = ('0' + $scope.dateFrom.getDate()).slice(-2);
+    var mm = ('0' + ($scope.dateFrom.getMonth() + 1)).slice(-2);
+    var yy = $scope.dateFrom.getFullYear();
+
+    var dateFromString = dd + '.' + mm + '.' + yy;
+
+
+    var dd = ('0' + $scope.dateTo.getDate()).slice(-2);
+    var mm = ('0' + ($scope.dateTo.getMonth() + 1)).slice(-2);
+    var yy = $scope.dateTo.getFullYear();
+
+    var dateToString = dd + '.' + mm + '.' + yy;
+
+    console.log(dateFromString);
+    console.log(dateToString);
+
+
+    //Получение списка групп
     $scope.getGroups = function () {
         $http({
             method: 'GET',
@@ -21,49 +95,52 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridGroupingConst
 
 
     $scope.getGroups();
+    //Получение списка групп
 
 
-    $scope.getStatSrez = function () {
-        $http({
-            method: 'GET',
-            url: 'http://18.140.232.52:8081/api/v1/slices/max'
-        }).then(function (value) {
-            $scope.statsrez = value.data.value;
-            console.log($scope.statsrez);
-        })
+
+
+    //Заказать формирование срезов
+    $scope.getDatas = function () {
+
+
     };
 
-    $scope.getStatSrez();
 
-
-    $scope.getDatas = function (dateFrom, dateTo, statsrez, item) {
-        console.log(dateFrom);
-        console.log(dateTo);
-        console.log(statsrez);
-        console.log(item)
-    };
+    //Заказать формирование срезов
 
 
     $scope.tree_data = [
-        {
-            Name: "Группа о преступности/правонарушениях", Area: 268581, Population: 26448193, TimeZone: "Mountain",
-            children: [
-                {
-                    Name: "California",
-                    children: [
-                        {Name: "San Francisco", Area: 231, Population: 837442, TimeZone: "PST"},
-                        {Name: "Los Angeles", Area: 503, Population: 3904657, TimeZone: "PST"}
-                    ]
+        {Name:"USA",Area:9826675,Population:318212000,TimeZone:"UTC -5 to -10",
+            children:[
+                {Name:"California", Area:423970,Population:38340000,TimeZone:"Pacific Time",
+                    children:[
+                        {Name:"San Francisco", Area:231,Population:837442,TimeZone:"PST"},
+                        {Name:"Los Angeles", Area:503,Population:3904657,TimeZone:"PST"}
+                    ],
+                    icons: {
+                        iconLeaf: "fa fa-sun-o"
+                    }
                 },
-                {
-                    Name: "Illinois", Area: 57914, Population: 12882135, TimeZone: "Central Time Zone",
-                    children: [
-                        {Name: "Chicago", Area: 234, Population: 2695598, TimeZone: "CST"}
+                {Name:"Illinois", Area:57914,Population:12882135,TimeZone:"Central Time Zone",
+                    children:[
+                        {Name:"Chicago", Area:234,Population:2695598,TimeZone:"CST"}
                     ]
                 }
-            ]
-        }
-        // {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"}
+            ],
+            icons: {
+                iconLeaf: "fa fa-flag",
+                iconCollapse: "fa fa-folder-open",
+                iconExpand: "fa fa-folder"
+            }
+        },
+        {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"},
+        {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"},
+        {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"},
+        {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"},
+        {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"},
+        {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"},
+        {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain"}
     ];
 
 

9a0281a9dd929fad00eca3ca27f678c70db2209f
web-dev <dias.07.04@gmail.com>
2019-11-22 20:40:23 +0600

    образовано дерево на первый запрос рисует все корректно дерево разворачивает папку на 1 клик
    (пока не разворачивает на 2ой клик )

--- controllers/index.js
+++ controllers/index.js
@@ -18,8 +18,48 @@ app.config(['$qProvider', function ($qProvider) {
 app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGridTreeViewConstants', function ($scope, $http, $rootScope, uiGridTreeBaseService) {
 
 
+  //Получение списка статусов
+  $scope.getStatus = function () {
+    $http({
+      method: 'GET',
+      url: 'http://18.140.232.52:8081/api/v1/RU/slices/statuses'
+    }).then(function (value) {
+      $scope.status = value.data;
+    })
+  };
+  $scope.getStatus();
+  //Получение списка статусов
+
+
+  // Получение списка групп
+  $scope.getGroups = function () {
+    $http({
+      method: 'GET',
+      url: 'http://18.140.232.52:8081/api/v1/ru/slices/groups'
+    }).then(function (value) {
+      $scope.groups = value.data;
+    })
+  };
+  $scope.getGroups();
+  //Получение списка групп
+
+
+  //Получить № статсреза
+  $scope.getStatSrez = function () {
+    $http({
+      method: 'GET',
+      url: 'http://18.140.232.52:8081/api/v1/ru/slices/max'
+    }).then(function (value) {
+      $scope.statsrez = value.data.value;
+      // console.log($scope.statsrez);
+    })
+  };
+
+  $scope.getStatSrez();
+  //Получить № статсреза
+
+
   var operBySrez = '<div  ' +
-    /* 'ng-hide="row.entity.statusCode == 0 || row.entity.statusCode == 6" ' +*/
     'ng-controller="modalOperBySrezCtrl" ' +
     'ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" ' +
     'class="ui-grid-cell-contents"> ' +
@@ -31,7 +71,7 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGri
 
   $scope.gridOptions = {
     enableColumnMenus: false,
-    showTreeExpandNoChildren: false,
+    showTreeExpandNoChildren: true,
     enableHiding: false,
 
     enableSorting: false,
@@ -57,10 +97,10 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGri
         cellTemplate: "<div class=\"ui-grid-cell-contents ng-binding ng-scope\" ng-style=\"{'padding-left': grid.options.treeIndent * row.treeLevel + 'px'}\">" +
           "<img id='changeImg' " +
           "ng-hide='row.treeLevel == 2' " +
-          "ng-click='grid.appScope.toggleFirstRow(rowRenderIndex); grid.appScope.$parent.toggleSecRow(row.treeLevel, grid, row);' " +
+          "ng-click='grid.appScope.toggleFirstRow(rowRenderIndex, row.treeLevel, row)' " +
           "style='width: 24px; margin: 0 10px' " +
           "src='./img/folder.png' " +
-          "alt=''>{{COL_FIELD CUSTOM_FILTERS}} - {{row.treeLevel}}</div>"
+          "alt=''>{{COL_FIELD CUSTOM_FILTERS}} - (TreeLevel) {{row.treeLevel}}</div>"
       },
       {
         name: 'id',
@@ -97,7 +137,13 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGri
       }
     ]
   };
+  $scope.gridOptions.onRegisterApi = function (gridApi) {
+    $scope.gridApi = gridApi;
+    $scope.gridApi.treeBase.on.rowExpanded($scope, function (row) {
+      console.log(row.entity)
+    })
 
+  };
 
   $scope.gridOptions.multiSelect = true;
 
@@ -134,26 +180,25 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGri
       $scope.showGrid = response.data;
       $scope.showGrid.forEach(function (data, index) {
         dataSet.push(data);
-        // console.log(dataSet[index].children);
-
-
         dataSet[index].children.forEach(function (status) {
-          // console.log(status);
           status.groupCode = dataSet[index].code;
-
         });
 
 
         $scope.gridOptions.data = [];
         writeoutNode(dataSet, 0, $scope.gridOptions.data);
       });
-      console.log(dataSet)
+      console.log(dataSet);
+
 
     });
 
-    $scope.info = {};
+    // $scope.info = {};
     $scope.gridOptions.onRegisterApi = function (gridApi) {
       $scope.gridApi = gridApi;
+      $scope.gridApi.treeBase.on.rowExpanded($scope, function (row) {
+        console.log(row.entity)
+      })
 
     };
   };
@@ -162,185 +207,50 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGri
   $scope.getAllSrez();
 
 
-  $scope.changeImg = function () {
+  $scope.toggleFirstRow = function (index, treeLevel, row) {
+    // console.log('first row index ' + index);
+    // console.log('first row treeLevel ' + treeLevel);
+    if (treeLevel === 0) {
+      // return $scope.toggleSecRow(index, treeLevel);
+      $scope.gridApi.treeBase.toggleRowTreeState($scope.gridApi.grid.renderContainers.body.visibleRowCache[index]);
+    } else {
+      console.log(row.entity);
 
 
-    var img2 = 'https://www.freeiconspng.com/uploads/orange-folder-full-icon-png-13.png',
-      img1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNGhpmNOG1joyk_s0v1KH229zGd6CpZ0axtXRT6c6pqW4FlB2b&s';
-
-    var imgElement = document.getElementById('changeImg');
-    imgElement.src = (imgElement.src === img1) ? img2 : img1;
-  };
-
-  /*$scope.gridOptions = {
-
-    showTreeExpandNoChildren: false,
-    enableRowSelection: true,
-    enableSelectAll: true,
-    selectionRowHeaderWidth: 35,
-    rowHeight: 45,
-    enableFiltering: false,
-    treeRowHeaderAlwaysVisible: false,
-    columnDefs: [
-
-      {
-        name: 'groupCode',
-        displayName: 'Код группы',
-        visible: false,
-        sort: {priority: 0, direction: 'asc'}
-      },
-      {
-        name: 'groupName',
-        displayName: 'Группы',
-        grouping: {groupPriority: 0},
-        sort: {priority: 0, direction: 'asc'},
-        width: '450',
-        cellTemplate: '<div><div ' +
-          'ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" ' +
-          'class="ui-grid-cell-contents" ' +
-          'title="TOOLTIP">' +
-          '<button class="btn btn-primary" ' +
-          'ng-click="grid.appScope.toggleFirstRow(rowRenderIndex, row.entity)">' +
-          '<i class="fa fa-folder"></i></button> {{COL_FIELD CUSTOM_FILTERS}}</div></div>'
-      },
-      {
-        name: 'statusCode',
-        displayName: 'Код статуса',
-        visible: false
-      },
-
-      {
-        name: 'year',
-        displayName: 'Год',
-        visible: false
-      },
-
-
-      {
-        name: 'statusName',
-        displayName: 'Статус',
-        grouping: {groupPriority: 0},
-        sort: {priority: 0, direction: 'desc'},
-        width: '250',
-        cellTemplate:
-          '<div>' + '<div  ' +
-          'ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" ' +
-          'class="ui-grid-cell-contents" ' +
-          'title="TOOLTIP">' +
-          '<button class="btn btn-primary" ' +
-          'ng-click="grid.appScope.$parent.toggleSecRow(rowRenderIndex, COL_FIELD, row.treeNode.children)"> ' +
-          '<i class="fa fa-folder"></i>' +
-          '</button> {{COL_FIELD CUSTOM_FILTERS}}' +
-          '</div>' +
-          '</div>'
-      },
-
-
-      {
-        name: 'id',
-        sort: {priority: 0, direction: 'asc'},
-        displayName: 'Номер среза',
-        width: '*',
-        cellTemplate: '<div ' +
-          ' ' +
-          'ng-controller="ModalControlCtrl" ' +
-          'ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" ' +
-          'class="ui-grid-cell-contents text-center"> <button ng-click="grid.appScope.open(row.entity)" ng-hide="row.treeLevel==0 || row.treeLevel == 1" type="button" class="btn btn-primary"> {{COL_FIELD CUSTOM_FILTERS}} </button> </div>'
-      },
-      {
-        name: 'period',
-        displayName: 'Период',
-        width: '*'
-      },
-      {
-        name: 'created',
-        displayName: 'Сформирован',
-        width: '*'
-      },
-      {
-        name: 'button',
-        displayName: 'Действие',
-        cellTemplate: operBySrez
-      }
-
-    ],
-
-    onRegisterApi: function (gridApi) {
-      $scope.gridApi = gridApi;
-    }
-  };*/
-
-
-  //Получение списка статусов
-  $scope.getStatus = function () {
-    $http({
-      method: 'GET',
-      url: 'http://18.140.232.52:8081/api/v1/RU/slices/statuses'
-    }).then(function (value) {
-      $scope.status = value.data;
-    })
-  };
-  $scope.getStatus();
-  //Получение списка статусов
-
-
-  // Получение списка групп
-  $scope.getGroups = function () {
-    $http({
-      method: 'GET',
-      url: 'http://18.140.232.52:8081/api/v1/ru/slices/groups'
-    }).then(function (value) {
-      $scope.groups = value.data;
-    })
-  };
-  $scope.getGroups();
-  //Получение списка групп
-
-
-  $scope.toggleFirstRow = function (index) {
-
-
-    $scope.gridApi.treeBase.toggleRowTreeState($scope.gridApi.grid.renderContainers.body.visibleRowCache[index]);
-  };
-
-
-  $scope.toggleSecRow = function (treeLevel, grid, row) {
-
-    if (row.entity.$$treeLevel === 1) {
-      var params = row.entity;
-
-      var groupCode = params.groupCode,
-        statusCode = params.code,
-        year = params.statusYear;
-
-
-
-
+      /*var groupCode = row.entity.groupCode,
+        statusCode = row.entity.code,
+        year = row.entity.statusYear;
+      // подгружает данные http
       $http({
         method: 'GET',
         url: 'http://18.140.232.52:8081/api/v1/RU/slices?deleted=false&groupCode=' + groupCode + '&statusCode=' + statusCode + '&year=' + year + ''
       }).then(function (value) {
-
         console.log(value.data);
+        var arr = $scope.gridOptions.data;
+        value.data.forEach(function (statusData) {
+          arr[0]['children'][0]['children'].push(statusData);
+        });
 
+        writeoutNode(arr, 0, $scope.gridOptions.data);
 
-        /*$scope.showBtn = value.data;
-
-        $scope.gridOptions.data = value.data;
-
-        $scope.gridApi.treeBase.toggleRowTreeState($scope.gridApi.grid.renderContainers.body.visibleRowCache[index]);*/
-
-
+        console.log($scope.gridOptions.data);
       }, function (reason) {
         console.log(reason)
       });
+      $scope.gridApi.treeBase.toggleRowTreeState($scope.gridApi.grid.renderContainers.body.visibleRowCache[1]);*/
     }
 
 
-
-
-
   };
+
+
+  /*$scope.toggleSecRow = function (index, treeLevel) {
+    console.log('open second row');
+    console.log('sec row index' + index);
+    console.log('sec row level' + treeLevel);
+    $scope.gridApi.treeBase.toggleRowTreeState($scope.gridApi.grid.renderContainers.body.visibleRowCache[index]);
+
+  };*/
 
 
   $scope.user = [];
@@ -391,21 +301,6 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGri
   };
 
 
-  //Получить № статсреза
-  $scope.getStatSrez = function () {
-    $http({
-      method: 'GET',
-      url: 'http://18.140.232.52:8081/api/v1/ru/slices/max'
-    }).then(function (value) {
-      $scope.statsrez = value.data.value;
-      // console.log($scope.statsrez);
-    })
-  };
-
-  $scope.getStatSrez();
-  //Получить № статсреза
-
-
   //Дата начала отчета по умолчанию 1 января 2019
   var fromTimestamp = 1546322400;
   $scope.dateFrom = new Date(fromTimestamp * 1000);
@@ -427,20 +322,8 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridGroupingConstants', 'uiGri
   var dateToString = dd + '.' + mm + '.' + yy;
 
 
-  //Получение списка групп
-  $scope.getGroups = function () {
-    $http({
-      method: 'GET',
-      url: 'http://18.140.232.52:8081/api/v1/ru/slices/groups'
-    }).then(function (value) {
-      $scope.groups = value.data;
-    })
-  };
-  $scope.getGroups();
-  //Получение списка групп
-
-
 }]);
+
 
 /**
  *  ModalControlCtrl

66ee3202869554f0e3059b75b2e635f10c76080c
Aidos Zhunusbekov <a.zhunusbekov.m@gmail.com>
2019-12-05 11:10:06 +0600

    some minor changes

--- controllers/app.module.js
+++ controllers/app.module.js
@@ -1,27 +1,30 @@
-﻿angular.module('app').config(function ($routeProvider) {
-    $routeProvider
-        .when("/controllers/index", {
-            templateUrl: "index.html"
-        })
-        .when("/controllers/civil", {
-            templateUrl: "civil.html"
-        })
+﻿angular.module("app").config(function($routeProvider) {
+  $routeProvider
+    .when("/controllers/index", {
+      templateUrl: "index.html"
+    })
+    .when("/controllers/civil", {
+      templateUrl: "civil.html"
+    })
 
-        .otherwise({redirectTo: '/'});
+    .otherwise({ redirectTo: "/" });
 });
 
-
-angular.module('app')
-    .service('ModalService', ['$log', '$scope', '$http', function ($log, $scope, $http) {
-        return {
-            getStatSrez: function () {
-                $http({
-                    method: 'GET',
-                    url: 'http://18.140.232.52:8081/api/v1/slices/max'
-                }).then(function (value) {
-                    $scope.statsrez = value.data.value;
-                    console.log($scope.statsrez);
-                })
-            }
-        }
-    }]);
+angular.module("app").service("ModalService", [
+  "$log",
+  "$scope",
+  "$http",
+  function($log, $scope, $http) {
+    return {
+      getStatSrez: function() {
+        $http({
+          method: "GET",
+          url: "http://18.140.232.52:8081/api/v1/slices/max"
+        }).then(function(value) {
+          $scope.statsrez = value.data.value;
+          console.log($scope.statsrez);
+        });
+      }
+    };
+  }
+]);
